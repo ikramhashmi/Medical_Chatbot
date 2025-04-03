18966816223ff90a42e4e274ffc6b60e756ae7e8
unknown <you@example.com>
2025-04-03 05:38:37 -0700

    book added

--- store_index.py
+++ store_index.py
@@ -1,6 +1,6 @@
 from src.helper import load_pdf,text_split,huggingfaceembedding
 from langchain_community.vectorstores import FAISS
-extracted_data=load_pdf(r"artifacts\\Black White Simple Blank Resignation Letter (1).pdf")
+extracted_data=load_pdf(r"artifacts\\Medical_book.pdf")
 text_chunks=text_split(extracted_data)
 embeddings=huggingfaceembedding()
 def Database(text_chunks=text_chunks,embeddings=embeddings):
